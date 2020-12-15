0538bdf874f4718c3b3715544ec87b6f9a2e407b
Amulya54495 <amulyab38804@gmail.com>
2020-12-15 18:04:08 +0530

    added a file of functionalinterface

---
+++ JavaFirstEx.java
@@ -0,0 +1,28 @@
+
+@FunctionalInterface
+interface JavaFirst {
+    void print();
+    
+    default void defaultprint() {
+        System.out.println("interface");
+    }
+    default void defaultprint1() {
+        System.out.println("interface2");
+  }
+    static void staticprint(){
+        System.out.println("static");
+    }
+}
+
+public class JavaFirstEx implements JavaFirst {
+    public void print() {
+       System.out.println("abstract");  
+    }
+   public static void main(String[] args) {
+        JavaFirstEx ex = new JavaFirstEx();
+        ex.print();
+        ex.defaultprint();
+        ex.defaultprint1();
+         JavaFirst.staticprint();
+    }
+}
